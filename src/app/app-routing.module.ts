e40a9a87a5dadfb4014ea30fa5fbb52e68b33b54
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-02 00:45:23 +0530

    Added redirection to login, added aditional services and models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
   //default route
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  //lazy loading
-  
-  { path: 'home', component: HomeComponent, loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
 
+  //lazy loading
+  { path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'home', component: HomeComponent, loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
 
    //wildcard route for page not found - should be last route
   { path: '**', component: NotFoundComponent }
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //for directing to the user home page
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
