58c862f020df24a8d2101a9734aa165429572f55
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-02 23:55:57 +0530

    login page completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/model/User';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  //declare variables
+  loginform!: FormGroup;
+  isSubmitted: boolean = false;
+  error: string = '';
 
-  constructor() { }
+  loginUser?: User = new User();
+
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    //create a reactive form
+    this.loginform = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  //get controls for validation
+  get formControls() {
+    return this.loginform.controls;
+  }
+
+  //functionality for login
+  loginCredentials(): void {
+    //setting value of isSubmitted to true
+    this.isSubmitted = true;
+
+    //check if form is valid
+    if (this.loginform.invalid) {
+      this.error = "Please enter username and password";
+      return;
+    }
+
+    //if form is valid
+    if (this.loginform.valid) {
+      this.error = "";
+      console.log(this.loginform.value);
+      //check validation using checking login functionality
+      this.authService.loginVerify(this.loginform.value)
+        .subscribe(response => {
+          console.log(response);
+          if (response.data == "INVALID CREDENTIALS") {
+            this.error = "Invalid username or password";
+          }
+          else if (response.data.role == 1) {
+            this.router.navigate(['/home/admin']);
+          }
+          else if (response.data.role == 2) {
+            this.router.navigate(['/home/user']);
+          }
+        });
+    }
   }
 
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/shared/model/User';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  //for authenticating the user
+  public loginVerify(user: User): Observable<any> {
+
+    //call webAPI for checking username and password
+    return this.httpClient.get<User>(environment.apiURL +
+      '/api/login/' + user.userName + '&' + user.password);
+  };
 }
