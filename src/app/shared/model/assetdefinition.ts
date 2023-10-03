07840f7cb465d7ca95cdb734790c8e92fe71b1ea
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-04 00:03:46 +0530

    fixed many more bugs in asset

--- src/app/shared/model/assetdefinition.ts
+++ src/app/shared/model/assetdefinition.ts
@@ -1,12 +1,12 @@
 import { Assetclass } from './assetclass';
 import { Asset } from './asset';
 export class AssetDefinition {
-    adId: number = 5;
+    adId: number = 0;
     adName: string = '';
     assetId: number = 0;
-    assettype:Asset=new Asset();
+    assettype: Asset = new Asset();
     assetClassId: number = 0;
-    assetclass:Assetclass=new Assetclass();
+    assetclass: Assetclass = new Assetclass();
     active: boolean = false;
 
 }
