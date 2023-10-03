78084111c116549c691398e7304222c94f0225cc
AkhilaAjith <akhilaajith321@gmail.com>
2023-10-03 16:39:09 +0530

    added vendor list, add, disable and edit

--- src/app/vendor/vendor-routing.module.ts
+++ src/app/vendor/vendor-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VendorAddComponent } from './vendor-add/vendor-add.component';
 import { VendorListComponent } from './vendor-list/vendor-list.component';
+import { VendorEditComponent } from './vendor-edit/vendor-edit.component';
 
 const routes: Routes = [
-  {path: 'add',component:VendorAddComponent},
-  {path: 'list',component:VendorListComponent}
+  //vendor-add
+  { path: 'add', component: VendorAddComponent },
+  //vendor-list
+  { path: 'list', component: VendorListComponent },
+  //vendor-edit
+  { path: 'edit/:id', component: VendorEditComponent }
 ];
 
 @NgModule({
