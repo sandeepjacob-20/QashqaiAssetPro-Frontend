df411c297dccb55f5e099f83c1caff8a9eca538d
irinsunny <irins17@gmail.com>
2023-10-03 18:37:35 +0530

    hope it works

--- src/app/purchase/purchase-listall/purchase-listall.component.ts
+++ src/app/purchase/purchase-listall/purchase-listall.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Purchase } from 'src/app/shared/model/purchase';
+import { PurchaseService } from 'src/app/shared/services/purchase.service';
 @Component({
   selector: 'app-purchase-listall',
   templateUrl: './purchase-listall.component.html',
   styleUrls: ['./purchase-listall.component.scss']
 })
 export class PurchaseListallComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(public purchaseService:PurchaseService, private router:Router) { }
 
   ngOnInit(): void {
+    this.purchaseService.getAllOrders();
   }
-
+   //getting order data
+   populateEmployeeData(purchase:Purchase){
+    this.purchaseService.formOrderData=Object.assign({},purchase)//converting employee(only string) to object as formdata is object
+  }
+  
+  
 }
