c2f9e6a162763183da993227c0e5f7db6f87945b
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-03 17:16:31 +0530

    fixed bug in vendors

--- src/app/shared/model/asset.ts
+++ src/app/shared/model/asset.ts
@@ -1,9 +1,10 @@
 import { Assetclass } from './assetclass';
+import { Assettype } from './assettype';
 export class Asset {
     adId: number = 0;
     adName: string = '';
     assetId: number = 0;
-    asset:Asset=new Asset();
+    assettype: Assettype =new Assettype();
     assetClassId: number = 0;
     assetclass:Assetclass=new Assetclass();
     active: boolean = false;
