8de0282b98f99b931d2a9c8267013b55a37538a9
delnamerin2 <delnamerincherian2001@gmail.com>
2023-10-03 21:39:29 +0530

    asset add componet done

--- src/app/shared/services/asset.service.ts
+++ src/app/shared/services/asset.service.ts
@@ -2,24 +2,68 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Asset } from '../model/asset';
+import { Observable } from 'rxjs';
+import { Assettype } from '../model/assettype';
+import { Assetclass } from '../model/assetclass';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssetService {
-  asset:Asset[];
+  formAssetData:Asset=new Asset();
+  assets:Asset[];
+  assettype: Assettype[];
+  assetclass: Assetclass[];
   constructor(private httpClient:HttpClient) { }
   getAllAsset():void{
     //getting the data
-    this.httpClient.get(environment.apiURL+'/api/asset')
+    this.httpClient.get(environment.apiURL+'/api/assetdefinition')
     .toPromise()
     .then(response =>{
       console.log(response);
-      this.asset=response as Asset[];
+      this.assets=response as Asset[];
     },
     error=>{
       console.log('Error')
     })
   }
+
+  getAllAssetType(): void{
+    this.httpClient.get(environment.apiURL+'/api/assetO')
+    .toPromise()
+    .then(response => {
+      console.log(response)
+      this.assettype = response as Assettype[]
+    },
+    error=>{
+      console.log(error)
+    });
+    
+  }
+
+  getAllAssetClass(): void{
+    this.httpClient.get(environment.apiURL+'/api/assetClass')
+    .toPromise()
+    .then(response => {
+      console.log(response)
+      this.assetclass = response as Assetclass[]
+    },
+    error=>{
+      console.log(error)
+    });
+    
+  }
+  //adding assetdefintion
+   insertAsset(asset:Asset):Observable<any>{
+    return this.httpClient.post(environment.apiURL+'/api/assetdefinition',asset)
+   }
+  //disable
+  disableAsset(id:number){
+    return this.httpClient.get(environment.apiURL+'/api/assetdefinition/deactivate/'+id)
+  }
+  // //update
+   updateAsset(asset:Asset):Observable<any>{
+   return this.httpClient.put(environment.apiURL+'/api/assetdefinition',asset)
+   }
 
 }
