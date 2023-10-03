2cf213007d09f0503ab353d750372c079c8af7a3
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-04 00:32:39 +0530

    added one page in assetmaster

--- src/app/assetmaster/assetmaster.module.ts
+++ src/app/assetmaster/assetmaster.module.ts
@@ -6,10 +6,11 @@ import { AssetmasterComponent } from './assetmaster.component';
 import { AssetmasterAddComponent } from './assetmaster-add/assetmaster-add.component';
 import { AssetmasterListComponent } from './assetmaster-list/assetmaster-list.component';
 import { PendingListComponent } from './pending-list/pending-list.component';
+import { NewordersComponent } from './neworders/neworders.component';
 
 
 @NgModule({
-  declarations: [AssetmasterComponent, AssetmasterAddComponent, AssetmasterListComponent, PendingListComponent],
+  declarations: [AssetmasterComponent, AssetmasterAddComponent, AssetmasterListComponent, PendingListComponent, NewordersComponent],
   imports: [
     CommonModule,
     AssetmasterRoutingModule
---
+++ src/app/assetmaster/neworders/neworders.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Purchase } from 'src/app/shared/model/purchase';
+import { PurchaseService } from 'src/app/shared/services/purchase.service';
+
+@Component({
+  selector: 'app-neworders',
+  templateUrl: './neworders.component.html',
+  styleUrls: ['./neworders.component.scss']
+})
+export class NewordersComponent implements OnInit {
+
+  constructor(public purchaseService: PurchaseService, private router: Router) { }
+
+  ngOnInit(): void {
+     this.purchaseService.getAllOrders();
+  }
+
+  //getting order data
+  populateOrderData(purchase: Purchase) {
+    this.purchaseService.formOrderData = Object.assign({}, purchase)//converting employee(only string) to object as formdata is object
+  }
+
+
+  //update
+  updateOrders(purchase: Purchase) {
+    console.log(purchase);
+    this.populateOrderData(purchase);
+    this.router.navigate(['/purchase/edit', purchase.pdId])
+  }
+
+}
