a8eef65a7f0e3ae50adf31d5b73da69e31937020
AkhilaAjith <akhilaajith321@gmail.com>
2023-10-03 22:08:08 +0530

    fixed bugs

--- src/app/vendor/vendor-routing.module.ts
+++ src/app/vendor/vendor-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { VendorAddComponent } from './vendor-add/vendor-add.component';
 import { VendorListComponent } from './vendor-list/vendor-list.component';
 import { AuthGuard } from '../auth/auth.guard';
+import { VendorEditComponent } from './vendor-edit/vendor-edit.component';
 
 const routes: Routes = [
   { path: 'add', component: VendorAddComponent, canActivate: [AuthGuard], data: { role: '1' } },
-  { path: 'list', component: VendorListComponent, canActivate: [AuthGuard], data: { role: '1' } }
+  { path: 'list', component: VendorListComponent, canActivate: [AuthGuard], data: { role: '1' } },
+  { path: 'edit/:id', component: VendorEdit Component, canActivate: [AuthGuard], data: { role: '1' } }
 ];
 
 @NgModule({
