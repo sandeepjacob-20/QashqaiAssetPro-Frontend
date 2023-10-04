f01936fb283f614b42bdeb5fa1c43ef9d8e46227
sandeep_jacob <sandeepjacob1@gmail.com>
2023-10-04 17:39:48 +0530

    added style improvements and login

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,13 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/shared/model/User';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private router:Router) { }
 
   //for authenticating the user
   public loginVerify(user: User): Observable<any> {
@@ -18,4 +20,17 @@ export class AuthService {
     return this.httpClient.get<User>(environment.apiURL +
       '/api/login/' + user.userName + '&' + user.password);
   };
+
+  // for logout
+  public logOut() {
+
+    //clear all local and session storage keys
+    localStorage.removeItem('USER_NAME');
+    localStorage.removeItem('ACCESS_ROLE');
+    localStorage.removeItem('JWT_TOKEN');
+
+    //redirect to login
+    this.router.navigate(['auth/login']);
+  }
+
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [SharedComponent, NotFoundComponent],
+  declarations: [SharedComponent, NotFoundComponent, HeaderComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
